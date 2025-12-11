b3ac169ad5ce7e61500e21225db8ca906c1f916d
Moonyoung1208 <mywky1103@gmail.com>
2025-12-11 11:25:57 +0900

    Remove database connection constants
    Removed database connection constants from config.php.

--- attendance/config.php
+++ attendance/config.php
@@ -1,11 +1,6 @@
 <?php
 // config.php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'gton'); 
-define('DB_PASSWORD', 'gt1103!@');
-define('DB_NAME', 'gton');
-
 // MySQLi 연결 객체 생성 함수
 function connectDB() {
     $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
